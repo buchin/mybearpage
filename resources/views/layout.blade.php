dc27873376f271924ce63215bf1d8122506e8ba4
Mochammad Masbuchin <buchin@dropsugar.com>
2020-10-20 21:50:09 +0700

    favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -10,6 +10,7 @@
 
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="{{ url('favicon.png') }}" type="image/png">
         @yield('head')
     </head>
     <body>
